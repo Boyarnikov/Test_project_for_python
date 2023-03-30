a48185934de835f359517b6328c16cfdc08ab5c7
Your Name <you@example.com>
2023-03-30 20:29:12 +0300

    Order, user and Items added

---
+++ Item.py
@@ -0,0 +1,16 @@
+class Item:
+    _name = ""
+    _cost = 0
+    _description = ""
+
+    def __init__(self, name):
+        self._name = name
+
+    def get_cost(self):
+        return self._cost
+
+    def __str__(self):
+        return f"Item {self._name} "
+
+    def __repr__(self):
+        return f"ITEM {self._name} "
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from User import User
+from Item import Item
 
 
-# Press the green button in the gutter to run the script.
+name = "Ivan"
 
-print_hi('Students')
+Ivan = User(name, "+7 999 999 99 99", "Uvan@gmail.com")
+order1 = Ivan.create_order()
+order1 = Ivan.create_order()
+order1 = Ivan.create_order()
+order1 = Ivan.create_order()
+order1.add_item(Item("ноутбук"))
+order1.add_item(Item("наушники"))
+order1.add_item(Item("лампа"))
+
+print(Ivan)
+print(Ivan.get_orders()[0])
+print(Ivan.get_orders()[0].get_items())
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
